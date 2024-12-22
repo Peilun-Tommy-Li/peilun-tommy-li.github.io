cfe13302bc9cf3ecb9d38c20e6c418349e94a37b
peilunli <peilun.li@vanderbilt.edu>
2024-12-21 23:03:58 -1000

    Dec, 21, 2024

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-
   // Scroll to top button ----------------------------------------------------------
-  // When the user scrolls down 20px from the top of the document, show the button
+  // When the user scrolls down 350px from the top of the document, show the button
   window.onscroll = function() {
-    scrollFunction()
+    scrollFunction();
   };
 
   function scrollFunction() {
@@ -15,75 +14,79 @@ $(document).ready(function() {
   }
 
   // When the user clicks on the button, scroll to the top of the document
-  function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  }
-
   $("#topper").on("click", function() {
-    $("html").animate({
-      scrollTop: 0
-    }, 400);
+    $("html, body").animate({ scrollTop: 0 }, 400);
   });
 
-  // tooltips function
-  $(function() {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
+  // Initialize tooltips
+  $('[data-toggle="tooltip"]').tooltip();
 
-  // Initally hide the read more div
-     $("#read-more").css("display", "none");
+  // Initially hide the read more div
+  $("#read-more").css("display", "none");
 
-     // Show more on click
-     $("#badge-more").on("click", function() {
+  // Show more/less on click
+  $("#badge-more").on("click", function() {
+    // Show/hide the div
+    $("#read-more").fadeToggle("fast");
+    // Toggle the button text between "more" and "less"
+    if ($(this).text() === "more") {
+      $(this).text("less");
+    } else {
+      $(this).text("more");
+    }
+  });
 
-        // Show/hide the div
-        $("#read-more").fadeToggle("fast");
+  // Popover function
+  $('[data-toggle="popover"]').popover();
 
-        // Change the button
-        if ($("#badge-more").text() == "more") {
-           $("#badge-more").text("less");
-        } else {
-           $("#badge-more").text("more");
-        }
+  // Toggle all accordion panels for printing
+  // - Single handler for .expander
+  $(".expander").on("click", function() {
+    if ($(this).text() === "show all") {
+      $(this).text("hide all");
+      // Show all accordions
+      $(".panel-collapse").collapse('show');
+      // Alternatively, if needed: $(".panel-collapse").addClass("in");
+    } else {
+      $(this).text("show all");
+      // Hide all accordions
+      $(".panel-collapse").collapse('hide');
+      // Alternatively, if needed: $(".panel-collapse").removeClass("in");
+    }
+  });
 
-     });
+  // Load navbar
+  $("#navbar-include").load("navbar.html", function() {
+    $("#home").removeClass("active");
+    $("#vitae").addClass("active");
+  });
 
-     // popover function
-     $('[data-toggle="popover"]').popover();
+  // Smooth scrolling for navigation links
+  $('a[href*="#"]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top - 60
+    }, 500, 'linear');
+  });
 
-     // open all accordion panels for possible rinting
-//      $(".expander").on("click", function() {
-//
-//         // Change the button
-//         if ($(".expander").text() == "show all") {
-//            $(".expander").text("hide all");
-//            $(".panel-collapse").addClass("in");
-//            $(".panel-default a").attr("aria-expanded", "true").removeClass("collapsed");
-//         } else {
-//            $(".expander").text("show all");
-//            $(".panel-collapse").removeClass("in");
-//            $(".panel-default a").attr("aria-expanded", "false").addClass("collapsed");
-//         }
-//
-//      });
+  // Scroll to top when #topper is clicked (this duplicates the earlier #topper handler but is okay if consistent)
+  $('#topper').click(function() {
+    $('html, body').animate({ scrollTop: 0 }, 500);
+    return false;
+  });
 
-     // open all accordion panels for possible printing or close
-   $(".expander").on("click", function() {
+  // Immediately show first section blocks
+  // Select the first .block-timeline and add 'in-view' to all .resume-block inside it
+  $('.block-timeline:first .resume-block').addClass('in-view');
 
-      if ($(".expander").text() === "show all") {
-
-        // Change the button text
-         $(".expander").text("hide all");
-         // show all accordions
-         $(".panel-collapse").collapse('show');
-
-      } else {
-        // Change the button text
-         $(".expander").text("show all");
-         // hide all accordions
-         $(".panel-collapse").collapse('hide');
+  // Add 'in-view' on scroll for subsequent sections
+  $(window).on('scroll', function() {
+    $('.resume-block').each(function() {
+      var elementTop = $(this).offset().top;
+      var viewportBottom = $(window).scrollTop() + $(window).height();
+      if (elementTop < viewportBottom - 100) {
+        $(this).addClass('in-view');
       }
-   });
-
+    });
+  });
 });
